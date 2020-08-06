889f6c9a0652fca317693a1ce07f8a527ab8bfc1
yaelimarkovitz <67628655+yaelimarkovitz@users.noreply.github.com>
2020-08-06 10:13:18 +0300

    Update BST.h

--- BST.h
+++ BST.h
@@ -21,20 +21,14 @@ typedef void(*PrintFunc)(TreeNode*);
 TreeNode* 	BuildBST();
 void		DestroyBST(TreeNode** root);
 void 		InsertNode(TreeNode** root, TreeNode* newNode);
-void        InsertNode(TreeNode** root, int val);
+void        	InsertNode(TreeNode** root, int val);
 void		PrintInOrder(TreeNode* root);
 void		PrintPreOrder(TreeNode* root);
 void		PrintPostOrder(TreeNode* root);
 void 		PrintBST(TreeNode* root, PrintFunc func = PrintInOrder);
-void        removeNode(TreeNode **root ,int val);
-int         getDepth(TreeNode* root);
-bool        isExists(TreeNode* root , int val);
-bool        isEqual(TreeNode* root1,TreeNode* root2) ;
-// TODO until 1.7.20:
-// 1. Consider print API redundancy.
-// 2. Allow to add a new element in a convenient manner (should the user know "TreeNode*)?
-// 3. Allow to remove an already existing element.
-// 4. Allow to check whether a certain value exists in your BST.
-// 5. Allow to get the BST's depth.
+void        	removeNode(TreeNode **root ,int val);
+int         	getDepth(TreeNode* root);
+bool        	isExists(TreeNode* root , int val);
+bool        	isEqual(TreeNode* root1,TreeNode* root2) ;
 
 #endif // __BST_H__
